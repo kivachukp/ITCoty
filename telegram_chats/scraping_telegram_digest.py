3790a2b74a662aaa8124919c68b81a073b995355
Solo-Anna <108534103+Solo-Anna@users.noreply.github.com>
2023-06-29 16:36:35 +0300

    290623-2

--- telegram_chats/scraping_telegram_digest.py
+++ telegram_chats/scraping_telegram_digest.py
@@ -25,25 +25,38 @@ class DigestParser():
         self.bot_dict = kwargs['bot_dict'] if 'bot_dict' in kwargs else None
         self.bot = self.bot_dict['bot']
         self.chat_id = self.bot_dict['chat_id']
-        self.channel = 'https://t.me/young_june'
         self.report = kwargs['report'] if 'report' in kwargs else None
         self.db = DataBaseOperations(report=self.report)
         self.links = digest_links
         self.helper = helper
 
+    async def main_start(self, limit_msg=3):
+
+        print('main_start')
+        print(self.links)
+        for url in self.links:
+            print(url)
+            messages = await self.get_all_messages(url, limit_msg)
+            # print(messages)
+
+        print('READY TO PRINT REPORT')
+        if self.report and self.helper:
+            try:
+                await self.report.add_to_excel(report_type='digest')
+                await self.helper.send_file_to_user(
+                    bot=self.bot,
+                    chat_id=self.chat_id,
+                    path=self.report.keys.report_file_path['digest'],
+                )
+            except Exception as ex:
+                print(f"Error: {ex}")
+
     async def get_all_messages(self, channel, limit_msg):
 
         print('GET MESSAGES FROM', channel)
-
-        self.count_message_in_one_channel = 1
-        block = False
         offset_msg = 0  # номер записи, с которой начинается считывание
-        # limit_msg = 1   # максимальное число записей, передаваемых за один раз
         all_messages = []  # список всех сообщений
-        total_messages = 0
-        total_count_limit = limit_msg  # значение 0 = все сообщения
         history = None
-
         new_text = f"<em>channel {channel}</em>"
 
         self.msg = await helper.send_message(
@@ -51,7 +64,6 @@ class DigestParser():
             chat_id=self.bot_dict['chat_id'],
             text=new_text
         )
-
 
         try:
             history = await self.client(GetHistoryRequest(
@@ -109,9 +121,7 @@ class DigestParser():
                 )
                 print(response)
                 if response:
-                    text = f"Vacancy FOUND in {pro} table\n{response[0][0][0:40]}"
                     status = "found in db by url"
-                    print(text)
                     if self.report:
                         self.report.parsing_report(
                             report_type = 'digest',
@@ -119,39 +129,17 @@ class DigestParser():
                             status=status, channel=channel, message_id=message_id, site=domain,
                         )
                         self.report.parsing_switch_next(report_type='digest', switch=True)
-                        return text
+                        return status
         try:
             parser = parser_sites.get(domain)
             if parser:
                 print('START PARSING VACANCY')
                 parser_response = await parser(report=self.report, bot_dict=self.bot_dict).get_content_from_one_link(
                     url)
-                text = ''
-                if parser_response['response_dict']:
-                    if parser_response['response']['vacancy']:
-                        text += f"Status: {parser_response['response']['vacancy'].capitalize()}\n"
-                    text += f"url: {parser_response['response_dict']['response_dict']['vacancy_url'] if 'vacancy_url' in parser_response['response_dict']['response_dict'] else '-'}\n"
-                    text += f"title: {parser_response['response_dict']['response_dict']['title'][:40]}\n"
-                    text += f"profession: {parser_response['response_dict']['response_dict']['profession']}\n"
-                    text += f"tags: {parser_response['response_dict']['response_dict']['full_tags'] if parser_response['response_dict']['response_dict']['full_tags'] else '-'}\n"
-                    text += f"anti_tags: {parser_response['response_dict']['response_dict']['full_anti_tags'] if parser_response['response_dict']['response_dict']['full_anti_tags'] else '-'}\n"
-                else:
-                    text = f"{parser_response['response']['vacancy']}\n" \
-                           f"profession: {parser_response['profession']['profession'] if parser_response['profession']['profession'] else '-'}\n" \
-                           f"tags: {parser_response['profession']['tag'] if parser_response['profession']['tag'] else '-'}\n" \
-                           f"anti_tags: {parser_response['profession']['anti_tag'] if parser_response['profession']['anti_tag'] else '-'}"
-                status = f"{parser_response['response']['vacancy']}"
-                # print(text)
-                if self.report:
-                    self.report.parsing_report(
-                        report_type='digest',
-                        link_current_vacancy=url,
-                        status=status, channel=channel, message_id=message_id, site=domain
-                    )
+                if parser_response['response']['vacancy']:
+                    status = f"{parser_response['response']['vacancy']}"
             else:
-                text = f"NO PARSER for {domain}"
                 status = "no parser"
-            # print(text)
             if self.report:
                 self.report.parsing_report(
                     report_type='digest',
@@ -159,9 +147,8 @@ class DigestParser():
                     status=status, channel=channel,
                     message_id=message_id, site=domain
                 )
-            if self.report:
                 self.report.parsing_switch_next(report_type='digest', switch=True)
-            return text
+            return status
         except Exception as e:
             return e
 
@@ -175,9 +162,6 @@ class DigestParser():
         for url_entity, inner_text in message.get_entities_text(MessageEntityTextUrl):
             url = url_entity.url
             print(url)
-            # if '/t.me/' in url or 'youtube' in url or 'forms.gle' in url:
-            #     pass
-            # else:
             res = re.findall(r'http[\:\/a-zA-Z0-9\.\=&-]*', url)
             if res:
                 urls += res
@@ -189,32 +173,11 @@ class DigestParser():
         print('URLS MORE', urls)
         for i in urls:
             print(i)
-            result = await self.db_check_add_single_vacancy(i, channel, message_id)
+            status = await self.db_check_add_single_vacancy(i, channel, message_id)
+            
 
-
-    async def main_start(self, limit_msg=3):
-
-        print('main_start')
-        print(self.links)
-        for url in self.links:
-            print(url)
-            messages = await self.get_all_messages(url, limit_msg)
-            # print(messages)
-
-        print('READY TO PRINT REPORT')
-        if self.report and self.helper:
-            try:
-                await self.report.add_to_excel(report_type='digest')
-                await self.helper.send_file_to_user(
-                    bot=self.bot,
-                    chat_id=self.chat_id,
-                    path=self.report.keys.report_file_path['digest'],
-                )
-            except Exception as ex:
-                print(f"Error: {ex}")
-
-
-async def main(report, client, bot_dict, action='get_message'):
-    get_messages = DigestParser(client=client, bot_dict=bot_dict, report=report)
-    print('START')
-    await get_messages.main_start(digest_links_test, limit_msg=20, action=action)  # get_participants get_message
+#
+# async def main(report, client, bot_dict, action='get_message'):
+#     get_messages = DigestParser(client=client, bot_dict=bot_dict, report=report)
+#     print('START')
+#     await get_messages.main_start(digest_links_test, limit_msg=20, action=action)  # get_participants get_message
